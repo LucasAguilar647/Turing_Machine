86139e90f5b28fb6be2437eab843ce3eccc1918b
Rodrigo Zordán <zordan.rodrigo97@gmail.com>
2025-06-23 18:37:06 -0300

    edit: estados invertir string 'abc'

--- Programa_invertir_string.sql
+++ Programa_invertir_string.sql
@@ -16,7 +16,7 @@ INSERT INTO programa VALUES
 ('load', 'a', 'load_a', 'X', 'R' ),
 ('load', 'b', 'load_b', 'X', 'R' ), 
 ('load', 'c', 'load_c', 'X', 'R' ), 
-('load', 'B', 'finish?', 'B', 'L' ),
+('load', 'B', 'finish', 'B', 'L' ),
 
 
 ('load_a', 'a', 'load_a', 'a', 'R'),
@@ -24,6 +24,18 @@ INSERT INTO programa VALUES
 ('load_a', 'c', 'load_a', 'c', 'R'),
 ('load_a', 'X', 'load_a', 'X', 'R'),
 ('load_a', 'B', 'search_X', 'a', 'L'), 
+
+('load_b', 'a', 'load_b', 'a', 'R'),
+('load_b', 'b', 'load_b', 'b', 'R'),
+('load_b', 'c', 'load_b', 'c', 'R'),
+('load_b', 'X', 'load_b', 'X', 'R'),
+('load_b', 'B', 'search_X', 'b', 'L'),
+
+('load_c', 'a', 'load_c', 'a', 'R'),
+('load_c', 'b', 'load_c', 'b', 'R'),
+('load_c', 'c', 'load_c', 'c', 'R'),
+('load_c', 'X', 'load_c', 'X', 'R'),
+('load_c', 'B', 'search_X', 'c', 'L'),
 
 
 ('search_X', 'a', 'search_X', 'a', 'L'),
@@ -34,5 +46,5 @@ INSERT INTO programa VALUES
 ('load_next', 'a', 'load_a', 'X', 'R'), 
 ('load_next', 'b', 'load_b', 'X', 'R'), 
 ('load_next', 'c', 'load_c', 'X', 'R'),
-('load_next', 'B', 'finish?', 'B', 'R'),
+('load_next', 'B', 'finish', 'B', 'R'),
 ('load_next', 'X', 'load_next', 'X', 'L')
